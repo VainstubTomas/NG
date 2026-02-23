69612114c1972094304b3442e61fc969d5f41067
Tomas Vainstub <vainstubtomas@gmail.com>
2026-02-23 15:57:47 -0300

    Add AppBar component for header and footer
    - Created a new AppBar component in appbar.jsx for the header, displaying the title and user name.
    - Added a footer component in footer.jsx with a static AppBar positioned at the bottom of the page.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,8 @@ import axios from 'axios'
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
-import Stack from '@mui/material/Stack';
+import ButtonAppBar from './components/appbar';
+import Footer from './components/footer';
 
 import './App.css'
 
@@ -26,36 +27,35 @@ function App() {
   }, []);
 
   return (
-    <>
-      <div className='container'>
+    <div className='container'>
+      <ButtonAppBar />
+      <div className='hero'>
         <h1>Open positions</h1>
-
-        <div className='mainComponent'>
-          {/* title */}
-          <section className='positionItemConatiner'>
-            {positionsList.map((p) => (
-              <div key={p.id} className='positionItem'>
-                <h3>{p.title}</h3>
-
-                {/* input */}
-                <Box
-                  component="form"
-                  sx={{ '& > :not(style)': { m: 1, width: '25ch' } }}
-                  noValidate
-                  autoComplete="off"
-                >
-                  <TextField id="outlined-basic" label="GitHub repository link" variant="outlined" />
-                </Box>
-
-                {/* submit */}
-                <Button variant="contained" style={{width:"140px"}}>Submit</Button>
-              </div>
-            ))
-            }
-          </section>
-        </div>
       </div>
-    </>
+      <section className='positionItemConatiner'>
+        {positionsList.map((p) => (
+          <div key={p.id} className='positionItem'>
+            <h3>{p.title}</h3>
+            <Box
+              component="form"
+              sx={{ width: '100%' }}
+              noValidate
+              autoComplete="off"
+            >
+              <TextField
+                id="outlined-basic"
+                label="GitHub repository link"
+                variant="outlined"
+                placeholder="Paste your repo here"
+                fullWidth
+              />
+            </Box>
+            <Button variant="contained" style={{ width: "140px" }}>Submit</Button>
+          </div>
+        ))}
+      </section>
+      <Footer />
+    </div>
   )
 }
 
