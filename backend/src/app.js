d1a3b92580726e7a1f26a3f9c2b132ac9ea0a16e
Tomas Vainstub <vainstubtomas@gmail.com>
2026-02-22 22:50:39 -0300

    chore: cleanup project structure

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,1 +1,10 @@
 //server configuration
+import http from "http";
+import express from "express";
+
+const app = express();
+const server = http.createServer(app);
+
+server.listen(8080, () => {
+    console.log("[APP] Server running on 8080 port.");
+});
